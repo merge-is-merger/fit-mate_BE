b5beb44dfae2d8ac4b3f7ddc7ff1aaba72815d51
Chaeminnn <134483661+Chaeminnn@users.noreply.github.com>
2024-07-12 20:25:26 +0900

    first commit

---
+++ src/test/java/com/Likelion12/fit_mate/FitMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Likelion12.fit_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
