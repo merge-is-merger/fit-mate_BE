640f61ec72da234307ec505685c97e25311d40e6
Chaeminnn <coalsjenny@gmail.com>
2024-08-02 17:20:30 +0900

    AuthController 수정
    import 부분 간소화

--- src/main/java/com/Likelion12/fit_mate/controller/AuthController.java
+++ src/main/java/com/Likelion12/fit_mate/controller/AuthController.java
@@ -9,10 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
